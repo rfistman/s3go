10f5dc89a4cae2498899a199f4fb13b106063c6a
Rhythmic Fistman <rfistman@gmail.com>
2013-06-11 13:39:04 +1000

    match string to sign

--- s3.go
+++ s3.go
@@ -10,19 +10,21 @@ type S3Request struct {
 	AWSSecretAccessKey string
 	httpVerb           string
 	args               map[string]string
+	bucket             string
+	resource           string
 }
 
 // http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
-func NewS3Request(httpVerb, host, name string) *S3Request {
+func NewS3Request(httpVerb, bucket, resource string) *S3Request {
 	m := map[string]string{
-		"Host":         host,
+		"Host":         bucket + ".s3.amazonaws.com",
 		"Content-MD5":  "",
 		"Content-Type": "",
 		// this looks right
 		"Date": time.Now().Format(time.RFC1123Z),
 	}
 
-	req := S3Request{httpVerb: httpVerb, args: m}
+	req := S3Request{httpVerb: httpVerb, args: m, bucket: bucket, resource: resource}
 	return &req
 }
 
@@ -34,7 +36,16 @@ func (req *S3Request) AuthorizationString() string {
 	return "AWS" + " " + req.AWSAccessKeyId + ":" + req.Signature()
 }
 
+func (req *S3Request) CanonicalizedAmzHeaders() string {
+	return ""
+}
+
+func (req *S3Request) CanonicalizedResource() string {
+	return "/" + req.bucket + req.resource
+}
+
 func (req *S3Request) StringToSign() string {
 	return req.httpVerb + "\n" + req.args["Content-MD5"] + "\n" + req.args["Content-Type"] + "\n" +
-		req.args["Date"]
+		req.args["Date"] + "\n" +
+		req.CanonicalizedAmzHeaders() + req.CanonicalizedResource()
 }
--- s3_test.go
+++ s3_test.go
@@ -4,7 +4,7 @@ package s3
 // http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
 
 import (
-	"log"
+	//	"log"
 	"testing"
 )
 
@@ -13,23 +13,19 @@ func Test_foo(t *testing.T) {
 	AWSAccessKeyId := "AKIAIOSFODNN7EXAMPLE"
 	AWSSecretAccessKey := "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
-	req := NewS3Request("GET", "johnsmith.s3.amazonaws.com", "/johnsmith/photos/puppy.jpg")
+	req := NewS3Request("GET", "johnsmith", "/photos/puppy.jpg")
 	req.AWSAccessKeyId = AWSAccessKeyId
 	req.AWSSecretAccessKey = AWSSecretAccessKey
 	req.args["Date"] = "Tue, 27 Mar 2007 19:36:42 +0000"
+
+	expectedStringToSign := "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg"
+	//log.Println(req.StringToSign())
+	if expectedStringToSign != req.StringToSign() {
+		t.Error("string to sign mismatch")
+	}
 
 	expectedAuthorizationString := `Authorization: AWS AKIAIOSFODNN7EXAMPLE: bWq2s1WEIj+Ydj0vQ697zp+IXMU=`
 	if expectedAuthorizationString != req.AuthorizationString() {
 		t.Error("authorization string mismatch")
 	}
-	expectedStringToSign := `GET\n
-\n
-\n
-Tue, 27 Mar 2007 19:36:42 +0000\n
-/johnsmith/photos/puppy.jpg`
-	log.Println(req.StringToSign())
-	if expectedStringToSign != req.StringToSign() {
-		t.Error("string to sign mismatch")
-	}
-
 }
