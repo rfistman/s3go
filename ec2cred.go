ee40ddcb10402a01effaca36c9e5ae1986591b19
Rhythmic Fistman <rfistman@gmail.com>
2013-06-13 13:26:39 +0000

    yay, listed a bucket on ec2

--- ec2cred.go
+++ ec2cred.go
@@ -3,29 +3,34 @@ package s3
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
+	//"log"
 	"net/http"
 )
 
-func GetEC2Credentials(role string) {
+type SecurityCredentials struct {
+	AWSAccessKeyId     string
+	AWSSecretAccessKey string
+	token              string
+}
+
+func GetEC2Credentials(role string) (*SecurityCredentials, error) {
 	url := "http://169.254.169.254/latest/meta-data/iam/security-credentials/" + role
 	//url := "http://localhost:1234/latest/meta-data/iam/security-credentials/" + role
-	log.Println(url)
 	//req, err := http.NewRequest("GET", url, nil)
 	res, err := http.Get(url)
 	if err != nil {
-		log.Println("get error: ", err)
-		return
+		return nil, err
 	}
 	defer res.Body.Close()
 	contents, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Println("read error: ", err)
-		return
+		return nil, err
 	}
-	log.Println("CREDS: ", string(contents))
-	var objmap map[string]string
-	err = json.Unmarshal(contents, &objmap)
-	log.Println(err, objmap)
 
+	//log.Println("CREDS: ", string(contents))
+	var m map[string]string
+	err = json.Unmarshal(contents, &m)
+	//log.Println(err, m)
+
+	return &SecurityCredentials{m["AccessKeyId"], m["SecretAccessKey"], m["Token"]}, nil
 }
--- ec2cred_test.go
+++ ec2cred_test.go
@@ -1,11 +1,49 @@
 package s3
 
 import (
-	//"log"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"testing"
 )
 
 func Test_Sumpin(t *testing.T) {
 	// t.Error("test sumpin")
-	GetEC2Credentials("blob_test_rw")
+	cred, err := GetEC2Credentials("blob_test_rw")
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	s3 := NewS3Request("GET", "/")
+	s3.AWSAccessKeyId = cred.AWSAccessKeyId
+	s3.AWSSecretAccessKey = cred.AWSSecretAccessKey
+	s3.args["x-amz-security-token"] = cred.token
+	s3.args["Host"] = "s3.amazonaws.com"
+
+	// could add it to map, but that would change this
+	auth := s3.AuthorizationString()
+
+	req, err := http.NewRequest(s3.httpVerb, "http://"+s3.args["Host"]+s3.resource, nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	for k, v := range s3.args {
+		req.Header.Add(k, v)
+	}
+	req.Header.Add("Authorization", auth)
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+
+	log.Println(string(contents), err)
 }
